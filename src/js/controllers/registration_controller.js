b5b5d01b44d341319000a9900e00c9482fabc502
KenjiRelut <kenji.relut@gmail.com>
2015-05-21 13:09:19 +0200

    recupération info nouvel utilisateur et try 2 method post

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -3,13 +3,22 @@ angular.module('Room8.controllers.Registration', [])
 .controller('registration_controller', function($scope,$http){
 	
 	$scope.User={};
+	$scope.test={
+		nom:'lol',
+		mail:'ahah@jj.com',
+		password:'ddd'
+	};
 	$scope.update = function(dataUser) {
 		$scope.User = angular.copy(dataUser);
-		/*$http({
+		$http({
 			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ User.name + '&mail=' + User.mail + '&mdp=' +User.password,
+			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ User.nom  + '&mdp=' +User.password + '&mail=' + User.mail,
 			headers: {'Accept': 'application/json'}
-		});*/
+		}).success(function(data){
+			alert('Well done');
+		}).error(function(data, status, headers, config){
+			alert(data, status, headers, config);
+		});
 	};
 	
 });
