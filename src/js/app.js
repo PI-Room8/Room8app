dd92fd4f7f25655c558b5cff432694fa83b75f63
LouLavo <lavorel.lou@gmail.com>
2015-05-21 10:33:55 +0200

    affichage newsfeed

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ angular.module('Room8', [
   $routeProvider.when('/Accounts', {templateUrl:'accounts.html',  reloadOnSearch: false});
   $routeProvider.when('/Chat', {templateUrl:'chat.html',  reloadOnSearch: false});
   $routeProvider.when('/ToDo', {templateUrl:'todo.html',  reloadOnSearch: false});
+  $routeProvider.when('/Registration', {templateUrl:'registration.html',  reloadOnSearch: false});
   $routeProvider.when('/Groceries', {templateUrl:'groceries.html',  reloadOnSearch: false});
   $routeProvider.when('/Settings', {templateUrl:'settings.html',  reloadOnSearch: false});
 });
--- src/js/controllers/news_controller.js
+++ src/js/controllers/news_controller.js
@@ -2,14 +2,13 @@ angular.module('Room8.controllers.News', [])
 
 .controller('NewsController', function($scope,$http){
 
-    var id =1;
-
 	$http({
         method: 'GET',
-        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/getAllNews?id=' + id,
+        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/getAllNews?id=1',
+        //params: {'id=1'},
         headers: {'Accept': 'application/json'}
      }).success(function(data){
-         $scope.myScopeData = data;
+         $scope.Liste = data;
     }).error(function(data, status, headers, config){
         alert(data, status, headers, config);
     });
