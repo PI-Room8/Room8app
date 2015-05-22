09c6778094b9b9be0202dcc5064691edb65715b2
KenjiRelut <kenji.relut@gmail.com>
2015-05-22 16:55:58 +0200

    Creer utilisateur done

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -6,16 +6,20 @@ angular.module('Room8.controllers.Registration', [])
 	$scope.User={};
 
 	$scope.update = function(dataUser) {
+		$scope.User = angular.copy(dataUser);
 		$http({
 			method:'POST',
 			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
-			headers: {'Accept': 'application/json'}
-		}).success(function(){
-			alert('Well done');
-		})/*.error(function(data, status, headers, config){
-			alert(data, status, headers, config);
-		});*/
+			 
+			headers: {'Accept': 'application/text'}
+		}).success(function(data){
+			alert(data);
+		}).error(function(data){
+			alert(data);
+		});
 
 	};
 
 });
+
+
