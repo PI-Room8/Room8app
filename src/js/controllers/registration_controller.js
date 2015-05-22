378110e93c4d255eeb9e82ee2c1d03c5941a8112
LouLavo <lavorel.lou@gmail.com>
2015-05-22 15:10:28 +0200

    Registration maj

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -2,40 +2,16 @@ angular.module('Room8.controllers.Registration', [])
 
 .controller('RegistrationController', function($scope,$http){
 	
-	$scope.User={};
-	$scope.test={
-		nom:'lol',
-		mail:'ahah@jj.com',
-		password:'ddd'
-	};
 	$scope.update = function(dataUser) {
-		$scope.User = angular.copy(dataUser);
 		$http({
 			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ User.nom  + '&mdp=' +User.password + '&mail=' + User.mail,
+			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' + dataUser.password + '&mail=' + dataUser.mail,
 			headers: {'Accept': 'application/json'}
 		}).success(function(data){
-			alert('Well done');
-		}).error(function(data, status, headers, config){
-			alert(data, status, headers, config);
+			alert(data);
+		}).error(function(data){
+			alert(data);
 		});
 	};
 
-/*
-	$scope.yolo={
-		nom: 'yolo',
-		mail: 'yolo@yo.lo',
-		mdp: 'yolo'
-	};
-
-	$http({
-        method: 'POST',
-        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addNews?text=nom='+ yolo.nom + '&mail=' + yolo.mail + '&mdp=' + yolo.mdp,
-		headers: {'Accept': 'application/json'}
-     }).success(function(){
-         alert('Well done');
-    }).error(function(){
-        alert('No');
-    });
-*/
 });
