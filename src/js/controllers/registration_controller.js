8870ae58a77db988e2f280850a16f22325ca8d81
LouLavo <lavorel.lou@gmail.com>
2015-05-22 15:43:47 +0200

    modif .js

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -1,20 +1,17 @@
 angular.module('Room8.controllers.Registration', [])
 
 .controller('RegistrationController', function($scope,$http){
-	
-
-	$scope.User={};
 
 	$scope.update = function(dataUser) {
 		$http({
 			method:'POST',
 			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
 			headers: {'Accept': 'application/json'}
-		}).success(function(){
-			alert('Well done');
-		})/*.error(function(data, status, headers, config){
-			alert(data, status, headers, config);
-		});*/
+		}).success(function(data){
+			alert(data);
+		}).error(function(data, status, headers, config){
+			alert(data);
+		});
 
 	};
 
