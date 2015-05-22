b40e6664b34357fdb6ad9a4db7cda446af28158b
KenjiRelut <kenji.relut@gmail.com>
2015-05-22 17:13:24 +0200

    passage des requetes back end en anglais nom fonction java

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -6,7 +6,7 @@ angular.module('Room8.controllers.Registration', [])
 		$scope.User = angular.copy(dataUser);
 		$http({
 			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
+			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addUser?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
 			headers: {'Accept': 'application/text'}
 		}).success(function(data){
 			alert(data);
