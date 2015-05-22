274f25e356244220ae490b914e7f2eb961362f2e
KenjiRelut <kenji.relut@gmail.com>
2015-05-22 15:21:17 +0200

    je retire home merci

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -3,22 +3,18 @@ angular.module('Room8.controllers.Registration', [])
 .controller('RegistrationController', function($scope,$http){
 	
 	$scope.User={};
-	$scope.test={
-		nom:'lol',
-		mail:'ahah@jj.com',
-		password:'ddd'
-	};
+
 	$scope.update = function(dataUser) {
 		$scope.User = angular.copy(dataUser);
 		$http({
 			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ User.nom  + '&mdp=' +User.password + '&mail=' + User.mail,
+			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
 			headers: {'Accept': 'application/json'}
-		}).success(function(data){
+		}).success(function(){
 			alert('Well done');
-		}).error(function(data, status, headers, config){
+		})/*.error(function(data, status, headers, config){
 			alert(data, status, headers, config);
-		});
+		});*/
 	};
 
 /*
