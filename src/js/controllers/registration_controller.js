eb783a1315ce8386702e26e4ae5182160368c03c
KenjiRelut <kenji.relut@gmail.com>
2015-05-26 17:51:17 +0200

    scrollable done pour newsfeed et registration

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -4,12 +4,12 @@ angular.module('Room8.controllers.Registration', [
 ])
 
 .controller('RegistrationController', function($scope,$http, $location){
-
+	$scope.User={}
 	$scope.update = function(dataUser) {
-		$scope.User=angular.copy(dataUser);
+		$scope.User = angular.copy(dataUser); 	
 		$http({
-			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addUser?nom='+ dataUser.nom  + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
+			method:'POST', 	method:'POST',
+			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addUser?nom='+ dataUser.nom + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
 			headers: {'Accept': 'application/text'}
 		}).success(function(data){
 			alert(data);
