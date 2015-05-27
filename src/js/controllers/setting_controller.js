3bc0ff7f539895d26687ace8cc2d574ed12e8cd1
LouLavo <lavorel.lou@gmail.com>
2015-05-27 10:19:28 +0200

    Indentation

--- src/js/controllers/setting_controller.js
+++ src/js/controllers/setting_controller.js
@@ -16,28 +16,28 @@ angular.module('Room8.controllers.Settings', [])
     });
 
     $scope.update = function(user) {
-    $http({
-        method: 'POST',
-        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + id + '&nom=' + user.nom_utilisateur + '&mdp=' + user.mot_de_passe + '&mail=' + user.adresse_mail,
-        headers: {'Accept': 'application/text'}
-     }).success(function(data){
-        if(data=='ok'){
-            alert('Your profile has been updated');
-            window.location.reload();
-        }
-        else{
-            alert('Try Again, something is wrong');
-            window.location.reload();
-        }
+        $http({
+            method: 'POST',
+            url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + id + '&nom=' + user.nom_utilisateur + '&mdp=' + user.mot_de_passe + '&mail=' + user.adresse_mail,
+            headers: {'Accept': 'application/text'}
+        }).success(function(data){
+            if(data=='ok'){
+                alert('Your profile has been updated');
+                window.location.reload();
+            }
+            else{
+                alert('Try Again, something is wrong');
+                window.location.reload();
+            }
         
          //window.location.href="/#/" : POUR REDIRECTION
-    }).error(function(data, status, headers, config){
-        alert(data, status, headers, config);
-    });
+        }).error(function(data, status, headers, config){
+            alert(data, status, headers, config);
+        });
     }
 
     $scope.refresh = function() {
-    window.location.reload();
+        window.location.reload();
     }
 
 });
