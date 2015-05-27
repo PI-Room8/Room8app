d18cadb6484e671a07d2412670b151fb04231e03
GabrielSchmitt <gabriel.schmitt9@gmail.com>
2015-05-27 11:28:59 +0200

    bonne version 27/05

--- src/js/controllers/setting_controller.js
+++ src/js/controllers/setting_controller.js
@@ -16,20 +16,28 @@ angular.module('Room8.controllers.Settings', [])
     });
 
     $scope.update = function(user) {
-        $http({
-            method: 'POST',
-            url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + id + '&nom=' + user.nom_utilisateur + '&mdp=' + user.mot_de_passe + '&mail=' + user.adresse_mail,
-            headers: {'Accept': 'application/text'}
-        }).success(function(data){
-            if(data=='ok'){
-                alert('Your profile has been updated');
-                window.location.reload();
-            }
-            else{
-                alert('Try Again, something is wrong');
-                window.location.reload();
-            }
-        
+    $http({
+        method: 'POST',
+        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + id + '&nom=' + user.nom_utilisateur + '&mdp=' + user.mot_de_passe + '&mail=' + user.adresse_mail,
+        headers: {'Accept': 'application/json'}
+     }).success(function(data){
+        if(data==1){
+            alert('Your profile has been updated');
+            window.location.reload();
+        }
+        else if(data==2){
+            alert('This pseudo is already being used');
+            window.location.reload();
+        }
+        else if(data==3){
+            alert('This mail address already has an account');
+            window.location.reload();
+        }
+            
+        else{
+            alert('Try Again, something is wrong');
+            window.location.reload();
+        }
          //window.location.href="/#/" : POUR REDIRECTION
         }).error(function(data, status, headers, config){
             alert(data, status, headers, config);
