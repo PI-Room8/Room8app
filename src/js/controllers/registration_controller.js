ad1ab4f56e87b95fcdc8fb0602483976833580c9
KenjiRelut <kenji.relut@gmail.com>
2015-05-27 10:45:52 +0200

    confirmer un password

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -1,22 +1,29 @@
 angular.module('Room8.controllers.Registration', [
 	'mobile-angular-ui.components.scrollable',
+	//'validation.match'
 	
 ])
+ 
 
-.controller('RegistrationController', function($scope,$http, $location){
-	$scope.User={}
+.controller('RegistrationController', function($scope,$http, $location,$route){
+	
 	$scope.update = function(dataUser) {
-		$scope.User = angular.copy(dataUser); 	
+		if(dataUser.password==dataUser.confirmPassword){	
 		$http({
 			method:'POST', 	method:'POST',
 			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addUser?nom='+ dataUser.nom + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
-			headers: {'Accept': 'application/text'}
+			headers: {'Accept': 'application/json'}
 		}).success(function(data){
 			alert(data);
 			$location.path('/');
 			$scope.$apply();
 		}).error(function(data){
 			alert(data);
-		});
+		});}else{alert("Pleace confirm your password!"); $route.reload();}
+	}
+	
+	$scope.connect=function(User){
+	
+	
 	}
 });
