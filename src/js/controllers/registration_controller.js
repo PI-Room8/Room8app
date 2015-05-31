0bfc65a116b5b2b65a03079e10e2fd61f598ed29
LouLavo <lavorel.lou@gmail.com>
2015-05-31 22:02:00 +0200

    Build android

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -56,44 +56,6 @@ angular.module('Room8.controllers.Registration', [
 		}
 	}
 	
-<<<<<<< HEAD
-	$scope.test={
-		nom:'lol',
-		mail:'ahah@jj.com',
-		password:'ddd'
-	};
-
-	$scope.update = function(dataUser) {
-		console.log();
-		$http({
-			method:'POST',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/creerUtilisateur?nom='+ dataUser.nom  + '&mdp=' + dataUser.password + '&mail=' + dataUser.mail,
-			headers: {'Accept': 'application/json'}
-		})/*.success(function(data){
-			alert(data);
-		}).error(function(data){
-			alert(data);
-		});*/
-	};
-
-/*
-	$scope.yolo={
-		nom: 'yolo',
-		mail: 'yolo@yo.lo',
-		mdp: 'yolo'
-	};
-
-	$http({
-        method: 'POST',
-        url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addNews?text=nom='+ yolo.nom + '&mail=' + yolo.mail + '&mdp=' + yolo.mdp,
-		headers: {'Accept': 'application/json'}
-     }).success(function(){
-         alert('Well done');
-    }).error(function(){
-        alert('No');
-    });
-*/
-=======
 	$scope.connect=function(User){
 
 		$http({
@@ -138,5 +100,5 @@ angular.module('Room8.controllers.Registration', [
 	$scope.connected=$rootScope.connected;
 	
 	}
->>>>>>> master
+
 });
