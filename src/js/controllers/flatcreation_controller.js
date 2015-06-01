8de8e159c66e9de30bdfa729d01824e3ec149fb3
Mszniber <zniber.m.soulaiman@gmail.com>
2015-06-01 09:30:39 +0200

    Flatcreation

--- src/js/controllers/flatcreation_controller.js
+++ src/js/controllers/flatcreation_controller.js
@@ -1,59 +1,34 @@
 angular.module('Room8.controllers.Flatcreation', [
 	'mobile-angular-ui.components.scrollable',
-	//'validation.match'
-	
 ])
 
 .controller('FlatcreationController', function($scope,$http, $location,$route, $rootScope){
 	
 if($rootScope.Connected==true){
 
-	$scope.create = function(dataUser) {
-		if(dataUser.password==dataUser.confirmPassword){	
-			$rootScope.UserName = dataUser.nom;
+	$scope.create = function(dataFlat) {
 
 			$http({
 				method:'POST', 	
-				url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/addUser?nom='+ dataUser.nom + '&mdp=' +dataUser.password + '&mail=' + dataUser.mail,
+				url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/createFlat?name='+ dataFlat.nom,
 				headers: {'Accept': 'application/json'}
 			}).success(function(data){
 				if(data==1){
-            		alert('Your profile has been created');
+            		alert('Your flat has been created');
 					
 					$location.path('/').replace;
 					$scope.$apply();
-				}else if(data==2){
-           			alert('This pseudo is already being used');
-           			window.location.reload();
-        		}else if(data==3){
-            		alert('This mail address already has an account');
-            		window.location.reload();
+				}else if(data==0){
+           			alert('This name is already being used');
         		}else{
-            		alert('Try Again, something is wrong');
+            		alert('Try Again, something went wrong');
             		window.location.reload();
-        		}}).error(function(data){
+        		}}).error(function(data, status, headers, config){
 					alert(data);
 				});
-		}else{
-			alert("Please confirm your password!"); 
-			$route.reload();
-		}
-	}
+        	}
 	
-	$scope.connect=function(User){
-
-		$http({
-			method:'GET',
-			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/getPassword?nom='+User.pseudo,
-			headers:{'Accept':'application/text'}
-		}).success(function(data){
-			alert(data);
-		}).error(function(data){
-			alert(data);
-		
-		});
 	
-	}
 
 }else{$location.path('/Registration').replace();}
 });
