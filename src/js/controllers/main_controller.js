98a3d5956c5bd19d96b6cab14cefbccaa2e56937
KenjiRelut <kenji.relut@gmail.com>
2015-06-01 10:51:27 +0200

    logout on going

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -2,13 +2,13 @@ angular.module('Room8.controllers.Main', [
 	'mobile-angular-ui.components.scrollable'
 ])
 
-.controller('MainController', function($rootScope,$http, $location){
+.controller('MainController', function($rootScope,$http, $location, $scope){
 	$rootScope.session=function(){
 		$http({
 			method:'GET',
 			url:'http://room8env-vgps3jicwb.elasticbeanstalk.com/getSession',
 			headers: {'Accept': 'application/json'}
-		}).Success(function(data){
+		}).success(function(data){
 			if(data!=""){
 				$rootScope.User=data;
 				$rootScope.Connected=true;
@@ -19,6 +19,18 @@ angular.module('Room8.controllers.Main', [
 		}).error(function(data, status,headers,config){
 			alert(data, status,headers,config);
 		});
-
-}
+		
+	
+	}
+	$scope.logout=function(){
+		$http({
+			method:'GET',
+			url:'http://room8env-vgps3jicwb.elasticbeanstalk.com/logout',
+			headers:{'Accept':'application/json'}
+		}).success(function(){
+			alert("Disconnected");
+		}).error(function(){
+			alert("Disconnection error")
+		});
+	}
 });
