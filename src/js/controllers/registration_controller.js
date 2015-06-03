010fa633874acc3a038cdd6d022b6291b15a9af9
Mszniber <zniber.m.soulaiman@gmail.com>
2015-06-03 09:10:32 +0200

    Papa maman les gars désolé

--- src/js/controllers/registration_controller.js
+++ src/js/controllers/registration_controller.js
@@ -72,35 +72,7 @@ angular.module('Room8.controllers.Registration', [
 				url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/login?name='+ User.pseudo +'&password='+ User.password,
 				headers:{'Accept':'application/json'}
 			}).success(function(data){
-				if(data==1){
-            		alert('Your profile has been created');
-
-            		$http({
-            			method:'GET',
-            			url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/getUser?name=' + dataUser.nom ,
-            			headers: {'Accept': 'application/json'}
-            		}).success(function(data2){	
-            			$rootScope.User=data2;
-            			console.log($rootScope.User);
-						$rootScope.Connected=true;
-						if ($rootScope.User.id_colocation == 0){
-							$location.path('/FindFlat').replace();
-						}
-						else {
-							$location.path('/Newsfeed').replace();
-						}
-            		}).error(function(data2){
-						alert('Can\'t get User');
-					});
-
-				}else if(data==2){
-           			alert('This pseudo is already being used');
-        		}else if(data==3){
-            		alert('This mail address already has an account');
-        		}else{
-            		alert('Try Again, something is wrong');
-        		}
-        	}).error(function(data, status,headers,config){
+				
 				if(data==""){
 					alert("Error : Your pseudo and your password don't match ");
 				}else{
