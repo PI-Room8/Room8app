8cb05fd126003a7f283ac6c4e455b1a0743f2bdb
GabrielSchmitt <gabriel.schmitt9@gmail.com>
2015-06-08 16:21:51 +0200

    HTML Gab + Money + variable.less

--- src/js/controllers/money_controller.js
+++ src/js/controllers/money_controller.js
@@ -3,6 +3,40 @@ angular.module('Room8.controllers.Money', [
     ])
 
 .controller('MoneyController', function($scope,$http, $rootScope, $location){
+
+                $scope.isVisible = false;
+                $scope.customStyle = {};
+
+                // I toggle the value of isVisible.
+                $scope.toggle = function() {
+
+                    $scope.isVisible = ! $scope.isVisible;
+
+                };
+
+                
+				$scope.color = function (amount){
+
+					if(amount>=0){
+						return {"color":"green"};
+					}
+					else{
+						return {"color":"red"};
+					}
+    			
+				}
+
+				$scope.text = function (amount){
+
+					if(amount>=0){
+						return "He paid";
+					}
+					else{
+						return "I paid";
+					}
+    			
+				}
 	
+
 
 });
