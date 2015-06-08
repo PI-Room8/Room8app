271eb793b16ccf71e05391944166602246aeb9c4
Mszniber <zniber.m.soulaiman@gmail.com>
2015-06-08 10:51:47 +0200

    Première requete comptes

--- src/js/controllers/money_controller.js
+++ src/js/controllers/money_controller.js
@@ -3,6 +3,16 @@ angular.module('Room8.controllers.Money', [
     ])
 
 .controller('MoneyController', function($scope,$http, $rootScope, $location){
+
+	$http({
+                	method:'GET',  
+                	url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/getAllsoldes?id=' + $rootScope.User.id_collocation,
+                	headers: {'Accept': 'application/json'}
+            	}).success(function(data){
+                	$scope.Solde = data;
+            	}).error(function(data){
+                	alert('Can\'t retrieve data');
+            	});
 	
 
 });
