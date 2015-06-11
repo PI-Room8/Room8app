7e9f49e5e4f5d90069e790ba08e3c9d57bbc746a
GabrielSchmitt <gabriel.schmitt9@gmail.com>
2015-06-11 17:12:44 +0200

    scan v2

--- src/js/directives/scan_directive.js
+++ src/js/directives/scan_directive.js
@@ -8,7 +8,6 @@ angular.module('Room8.directives.Scan', [
       require: 'ngModel',
       link: function(scope, elm, attrs, ctrl) {
 
-
          elm.on('click', function() {
             navigator.camera.getPicture(function (imageURI) {
                scope.$apply(function() {
@@ -18,8 +17,8 @@ angular.module('Room8.directives.Scan', [
                ctrl.$setValidity('error', false);
             }, { 
                 quality : 50,
-                destinationType : Camera.DestinationType.DATA_URL,
-                sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+                destinationType : Camera.DestinationType.FILE_URI,
+                sourceType : Camera.PictureSourceType.CAMERA,
                 allowEdit : true,
                 encodingType: Camera.EncodingType.JPEG,
                 targetWidth: 1000,
@@ -27,7 +26,55 @@ angular.module('Room8.directives.Scan', [
                 popoverOptions: CameraPopoverOptions,
                 saveToPhotoAlbum: false 
             })
+
+
+
+
          });  
       }
    };
 });
+
+
+/*navigator.camera.getPicture (onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI }) ;
+
+function onSuccess(imageURI) {var image = document.getElementById('myImage') ;
+    image.SRC = imageURI ;
+} function onFail(message) {alert ( 'a échoué car: '+ message);}
+
+/////////////////////////
+
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  // alert("ready-----")
+  pictureSource = navigator.camera.PictureSourceType;
+  destinationType = navigator.camera.DestinationType;
+}
+
+function capturePhoto() {
+  alert(navigator.camera);
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+    quality: 20,
+    destinationType: destinationType.FILE_URI,
+    targetWidth: 200,
+    targetHeight: 200,
+    saveToPhotoAlbum: true,
+    sourceType: pictureSource.CAMERA
+  });
+}
+
+function onPhotoDataSuccess(imageURI) {
+  // alert("success--");
+  var smallImage = document.getElementById('smallImage');
+  smallImage.style.display = 'block';
+  smallImage.src = imageURI;
+  // smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+  alert('Failed because: ' + message);
+}*/
