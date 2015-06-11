2f6e2d802b7cb02cf0dea550fd3a29dbbaa0f626
GabrielSchmitt <gabriel.schmitt9@gmail.com>
2015-06-11 17:20:38 +0200

    gabtest1

--- plugins/cordova-plugin-whitelist/src/ios/CDVNavigationWhitelistPlugin.h
+++ plugins/cordova-plugin-whitelist/src/ios/CDVNavigationWhitelistPlugin.h
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
+/*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
  distributed with this work for additional information
@@ -16,11 +15,17 @@
  KIND, either express or implied.  See the License for the
  specific language governing permissions and limitations
  under the License.
--->
-<widget xmlns     = "http://www.w3.org/ns/widgets"
-        id        = "io.cordova.helloCordova"
-        version   = "2.0.0">
+ */
 
-    <!-- Preferences for Android -->
-    <preference name="loglevel" value="DEBUG" />
-</widget>
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVWhitelist.h>
+
+@interface CDVNavigationWhitelistPlugin : CDVPlugin {}
+
+@property (nonatomic, readonly, strong) CDVWhitelist* whitelist; // readonly for public
+
+- (BOOL)shouldAllowNavigationToURL:(NSURL *)url;
+- (BOOL)shouldAllowRequestForURL:(NSURL *)url;
+
+@end
