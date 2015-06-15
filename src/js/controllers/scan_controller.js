c9c9d49cc49f658f05142da4d9cbf7e6b416f180
KenjiRelut <kenji.relut@gmail.com>
2015-06-15 11:32:16 +0200

    scan patch

--- src/js/controllers/scan_controller.js
+++ src/js/controllers/scan_controller.js
@@ -13,26 +13,11 @@ angular.module('Room8.controllers.Scan', [
      		myPictures.push(value);
    			}
 		}, true);
+		$location.path('/Scan').replace();
 
 
 	
 
 })
 
-/*.factory('Camera', ['$q', function($q) {
 
-  return {
-    getPicture: function(options) {
-      var q = $q.defer();
-
-      navigator.camera.getPicture(function(result) {
-        // Do any magic you need
-        q.resolve(result);
-      }, function(err) {
-        q.reject(err);
-      }, options);
-
-      return q.promise;
-    }
-  }
-}]);*/
