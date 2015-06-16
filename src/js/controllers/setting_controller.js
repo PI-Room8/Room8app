a781999639abde9cb62c4d014864200d37e5905f
KenjiRelut <kenji.relut@gmail.com>
2015-06-16 18:11:00 +0200

    ajustement settings

--- src/js/controllers/setting_controller.js
+++ src/js/controllers/setting_controller.js
@@ -7,33 +7,40 @@ angular.module('Room8.controllers.Settings', [
     if($rootScope.User.id_utilisateur != '0'){
 
         $scope.User = $rootScope.User;
-
+        //$scope.User.mot_de_passe="";
+		$scope.User.oldpassword=$rootScope.User.mot_de_passe;
         $scope.update = function(User) {
-            $http({
-                method: 'POST',
-                url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + User.id_utilisateur + '&nom=' + User.nom_utilisateur  + '&mdp=' + User.mot_de_passe + '&mail=' + User.adresse_mail,
-                headers: {'Accept': 'application/json'}
-            }).success(function(data){
-                if(data==1){
-                    $rootScope.User.nom_utilisateur = User.nom_utilisateur;
-                    $rootScope.User.mot_de_passe = User.mot_de_passe;
-                    $rootScope.User.adresse_mail = User.adresse_mail;
-                    alert('Your profile has been updated');
-                }
-                else if(data==2){
-                    alert('This pseudo is already being used');
-                }
-                else if(data==3){
-                    alert('This mail address already has an account');
-                }
-                else{
-                    alert('Try Again, something is wrong');
-                }
-                //window.location.href="/#/" : POUR REDIRECTION
-            }).error(function(data, status, headers, config){
-                alert(data, status, headers, config);
-            });
-        }
+			if($scope.User.mot_de_passe==$scope.User.oldpassword && $scope.User.confirmNewPassword==$scope.User.nouveau_mot_de_passe){
+	            $http({
+	                method: 'POST',
+	                url: 'http://room8env-vgps3jicwb.elasticbeanstalk.com/updateUser?id=' + User.id_utilisateur + '&nom=' + User.nom_utilisateur  + '&mdp=' + User.nouveau_mot_de_passe + '&mail=' + User.adresse_mail,
+	                headers: {'Accept': 'application/json'}
+	            }).success(function(data){
+	                if(data==1){
+	                    $rootScope.User.nom_utilisateur = User.nom_utilisateur;
+	                    $rootScope.User.mot_de_passe = User.nouveau_mot_de_passe;
+	                    $rootScope.User.adresse_mail = User.adresse_mail;
+	                    alert('Your profile has been updated');
+	                }
+	                else if(data==2){
+	                    alert('This pseudo is already being used');
+	                }
+	                else if(data==3){
+	                    alert('This mail address already has an account');
+	                }
+	                else{
+	                    alert('Try Again, something is wrong');
+	                }
+	                //window.location.href="/#/" : POUR REDIRECTION
+	            }).error(function(data, status, headers, config){
+	                alert(data, status, headers, config);
+	            });
+			}else{
+				alert("Error: You must insert your old password and confirm your new password");
+				$location.path('/Settings').replace();
+			}
+        
+		}
 
         
         $scope.leaveFlat=function(){
@@ -50,14 +57,12 @@ angular.module('Room8.controllers.Settings', [
 		
 		}
 
-
-    }else{
-        $location.path('/').replace();
-    }
-
-    $scope.refresh = function() {
-        $location.path('/Settings').replace();
-    }
+		$scope.refresh = function() {
+	        $location.path('/Settings').replace();
+	    }
+	} else{
+		$location.path('/').replace();
+	}
 
 });
 
